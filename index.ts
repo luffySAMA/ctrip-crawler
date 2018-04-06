48565c3c0724613be3f070c230db335745b17080
蔡华鹏 <caihuapeng@chp-mbp.lan>
2018-04-06 21:33:13 +0800

    登录携程

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import * as path from 'path';
 import { promisify } from 'util';
 import { Task } from './src/task/task';
 import { Schedule } from './src/task/schedule';
+import { Browser } from 'puppeteer';
 
 let stat = promisify(fs.stat);
 let mkdir = promisify(fs.mkdir);
@@ -16,8 +17,9 @@ let readFile = promisify(fs.readFile);
     await mkdir(resultFolder);
   }
   const browser = await puppeteer.launch({
-    headless: false
+    headless: true
   });
+  await login(browser);
   let fs1 = await readFile(path.join(__dirname, '../airport/important-airport.txt'));
   let fs2 = await readFile(path.join(__dirname, '../airport/china-airport.txt'));
   let fromList = fs1.toString().split('\n');
@@ -39,5 +41,18 @@ let readFile = promisify(fs.readFile);
     // await task.run('tao', 'syd', '2018-04-17');
   } catch (error) {
     console.log(error);
+  }
+
+  async function login(browser: Browser) {
+    const page = await browser.newPage();
+    await page.goto('https://passport.ctrip.com/user/login?BackUrl=http%3A%2F%2Fwww.ctrip.com%2F');
+    let username = '18936040203';
+    let password = '18936040203';
+    await page.type('#nloginname', username, { delay: 0 });
+    await page.type('#npwd', password, { delay: 0 });
+    let button = await page.$('#nsubmit');
+    await button.click();
+    await page.waitFor(500);
+    await page.close();
   }
 })();
